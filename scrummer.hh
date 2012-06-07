7f6d1613231303e0c0c23e01aeee965394727ae7
alexhairyman <alexhairyman@gmail.com>
2012-06-07 15:33:25 -0600

    whoopsy :)

--- scrummer.hh
+++ scrummer.hh
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2011 Henrik Cooke, henrik@cooke.se
+Copyright (c) 2012 Alex Herrmann alexhairyman@gmail.com
 
 Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the
